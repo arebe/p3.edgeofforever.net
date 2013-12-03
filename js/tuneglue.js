96fd945fe8749620449f5b61fd827ce4adf46371
Rachel Boyce <rfboyce@gmail.com>
2013-12-03 14:06:53 -0500

    static page elements in place

--- js/tuneglue.js
+++ js/tuneglue.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 	console.log('ready!');
-	$('#canvas').css('background-image', '#wheel_background');
+	$('#canvas').addClass('wheel_background');
+
+	var bpm_incoming = 180;
+	$('#bpm_incoming span').html(bpm_incoming);
+
+	var key_incoming = 'A flat';
+	$('#key_incoming span').html(key_incoming);
+
+	var transition_time = '90 sec';
+	$('#transition_time span').html(transition_time);
+
 });
